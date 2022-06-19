507bec03ee1630f38200bb72e51c3fb90716d129
Joshua <joshua@grimmett.com.au>
2022-06-20 05:50:05 +1000

    Videos and thumbnails

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ let config = {
         description: 'Watch the latest documentaries and videos on the Choice.',
         domain: 'media.amongoose.com'
     },
-    videosDirectory: '/Users/jooshhg/Documents/Development/Commercial/Mongoose/mongoose.tv/videos/',
-    thumbnailsDirectory: '/Users/jooshhg/Documents/Development/Commercial/Mongoose/mongoose.tv/public/thumbnails/'
+    videosDirectory: '/media/webadmin/media/videos',
+    thumbnailsDirectory: '/media/webadmin/media/thumbnails'
 }
 
 if (process.env.NODE_ENV === 'development') {
