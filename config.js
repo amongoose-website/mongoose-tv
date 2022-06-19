a4cfe6aeb6ac2cc786021cf847998d31495f8f37
Joshua <joshua@grimmett.com.au>
2022-06-19 19:38:30 +1000

    Added thumbnails directory

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ let config = {
         description: 'Watch the latest documentaries and videos on the Choice.',
         domain: 'media.amongoose.com'
     },
-    videosDirectory: '/Users/jooshhg/Documents/Documents - Josh’s MacBook Air/Development/Commercial/Mongoose/mongoose.tv/videos/'
+    videosDirectory: '/Users/jooshhg/Documents/Documents - Josh’s MacBook Air/Development/Commercial/Mongoose/mongoose.tv/videos/',
+    thumbnailsDirectory: '/Users/jooshhg/Documents/Documents - Josh’s MacBook Air/Development/Commercial/Mongoose/mongoose.tv/thumbnails/'
 } 
 
 if (process.env.NODE_ENV === 'development') {
