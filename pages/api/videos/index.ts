b9271a1b8b30ade475c45f663dddf8a311a8b254
Joshua <joshua@grimmett.com.au>
2022-06-21 12:14:10 +1000

    Try this

--- pages/api/videos/index.ts
+++ pages/api/videos/index.ts
@@ -3,14 +3,10 @@ import path from 'path'
 
 import appConfig from '../../../config'
 
-const CHUNK_SIZE_IN_BYTES = 1000000
 const VIDEOS_DIRECTORY = appConfig.videosDirectory
 
 export default function handler(req: any, res: any) {
   let range = req.headers.range
-  if (range == 'bytes=0-1') {
-    range = 'bytes=0-'
-  }
 
   if (!range) return res.status(400).end('Range must be provided')
 
@@ -20,12 +16,11 @@ export default function handler(req: any, res: any) {
   if (!fs.existsSync(videoPath)) return res.status(404)
   const videoSizeInBytes = fs.statSync(videoPath).size
 
-  const chunkStart = Number(range.replace(/\D/g, ''))
-  // Limit video to end of video file
-  const chunkEnd = Math.min(
-    chunkStart + CHUNK_SIZE_IN_BYTES,
-    videoSizeInBytes - 1
-  )
+  const parts = range.replace(/bytes=/, "").split("-");
+  const chunkStart = parseInt(parts[0], 10);
+  const chunkEnd = parts[1]
+      ? parseInt(parts[1], 10)
+      : videoSizeInBytes-1;
 
   const contentLength = chunkEnd - chunkStart + 1
 
