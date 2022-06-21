61516da6e2238b05f123b8ea8247a4cda490d64a
Joshua <joshua@grimmett.com.au>
2022-06-21 19:53:03 +1000

    New loading design

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,8 +3,10 @@ import Layout from "./Layout"
 const Loading = () => {
     return (
         <Layout>
-          <div className='container mx-auto bg-white rounded dark:bg-zinc-800 dark:text-white pt-5'>
-            <p className='text-center'>Loading...</p>
+          <div 
+            className='container mx-auto bg-white rounded dark:bg-zinc-800 dark:text-white pt-5 flex flex-col justify-center items-center'
+            style={{height: 'calc(100vh - 93.5px)'}}>
+              <h1 className='text-xl font-bold my-2 text-center opacity-7'>Loading...</h1>
           </div>
         </Layout>
       )
