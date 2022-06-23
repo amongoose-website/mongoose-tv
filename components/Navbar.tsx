5008865756e53035e0381235af72db6def22b88b
Joshua <joshua@grimmett.com.au>
2022-06-23 12:06:13 +1000

    Updated search bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,6 +16,27 @@ const uppercase = (old?: string | null) => {
     }).join(' ')
 }
 
+const SearchBar = () => {
+    return <form method='get' action='/results'>
+        <input 
+            id="search"
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect="off"
+            name="query"
+            tabIndex={0}
+            type='search'
+            spellCheck='false'
+            placeholder='Search'
+            aria-label='Search'
+            aria-haspopup='false'
+            aria-autocomplete="list"
+            dir="ltr"
+            className='block p-2 pl-10 w-full text-zinc-900 bg-zinc-50 rounded-lg border border-zinc-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none'
+    />
+</form>
+}
+
 
 function MenuLink({href, text, children, type}: {href: string, text?: string, children?: any, type?: string}) {
     const router = useRouter()
@@ -48,7 +69,7 @@ function MenuLink({href, text, children, type}: {href: string, text?: string, ch
 
 
 const Navbar = () => {
-    const [menuHidden, setMenuHidden] = useState(true)
+    const [menu, setMenu] = useState('closed')
     const [userMenu, setUserMenu] = useState(false)
     const siteMetadata = useSiteMetadata()
     
@@ -73,26 +94,22 @@ const Navbar = () => {
                                     clipRule="evenodd"></path>
                             </svg>
                         </div>
-                        <form method='get' action='/results'>
-                            <input type="text" id="search-navbar" name='query'
-                                className="block p-2 pl-10 w-full text-zinc-900 bg-zinc-50 rounded-lg border border-zinc-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Search..."/>
-                        </form>
+                        <SearchBar/>
                     </div>
-                    <button type="button" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3"
+                    <button id="searchButton" type="button" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3"
                         aria-expanded="false"
                         className="md:hidden text-zinc-500 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-zinc-700 focus:outline-none focus:ring-4 focus:ring-zinc-200 dark:focus:ring-zinc-700 rounded-lg text-sm p-2.5 mr-1"
-                        onClick={() => setMenuHidden(!menuHidden)}>
+                        onClick={() => setMenu(menu !== 'openSearch' ? 'openSearch' : 'closed')}>
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd"
                                 d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                                 clipRule="evenodd"></path>
                         </svg>
                     </button>
-                    <button data-collapse-toggle="mobile-menu-3" type="button"
+                    <button id='hamburgerButton' data-collapse-toggle="mobile-menu-3" type="button"
                         className="inline-flex items-center p-2 text-sm text-zinc-500 rounded-lg md:hidden hover:bg-zinc-100 focus:outline-none focus:ring-2 focus:ring-zinc-200 dark:text-zinc-400 dark:hover:bg-zinc-700 dark:focus:ring-zinc-600"
                         aria-controls="mobile-menu-3" aria-expanded="false"
-                        onClick={() => setMenuHidden(!menuHidden)}>
+                        onClick={() => setMenu(menu !== 'openMenu' ? 'openMenu' : 'closed')}>
                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd"
                                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
@@ -105,8 +122,8 @@ const Navbar = () => {
                         </svg>
                     </button>
                 </div>
-                <div className={`${menuHidden ? 'hidden' : ''} justify-between items-center w-full md:flex md:w-auto md:order-1`} id="mobile-menu-3">
-                    <div className="relative mt-3 md:hidden">
+                <div className={`justify-between items-center w-full md:flex md:w-auto md:order-1`} id="mobile-menu-3">
+                    <div id='searchBar' className={`relative mt-3 md:hidden ${menu === 'openSearch' ? '' : 'hidden'}`}>
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-zinc-500" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -115,11 +132,9 @@ const Navbar = () => {
                                     clipRule="evenodd"></path>
                             </svg>
                         </div>
-                        <input type="text" id="search-navbar"
-                            className="block p-2 pl-10 w-full text-zinc-900 bg-zinc-50 rounded-lg border border-zinc-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search..."/>
+                        <SearchBar/>
                     </div>
-                    <ul className="flex flex-col items-center gap-2 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                    <ul className={`${menu === 'openMenu' ? '' : 'hidden'} flex flex-col items-center gap-2 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium`}>
                         {/* <MenuLink href='/upload' text='Upload'/> */}
                         {user && <>
                             <div className='md:hidden'>
